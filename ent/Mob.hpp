535a8d39e0a96a0323def3cdfcd658b93f125d3a
Microcoding <cjaf.microcode@outlook.com>
2015-03-22 22:27:03 -0400

    More constructor parameters

--- ent/Mob.hpp
+++ ent/Mob.hpp
@@ -4,22 +4,23 @@
 #include "Entity.hpp"
 
 namespace entity {
-	namespace mob { 
-		
-		
+	namespace mob {
+
+
 		class Mob : public Entity {
-			
+
 		public:
-			Mob(int id, SDL_Rect* box);
+			Mob(int id, SDL_Rect* box, int speed, int health);
 			void render() override;
 			int getID();
 			int getSpeed();
 			double getHP();
-			
+
 		protected:
-			double hp;
-			int id;
-			int speed;
+			double p_hp;
+			int p_speed;
+			int p_id;
+			SDL_Rect* p_box;
 		};
 	}
 }
--- ent/Player.hpp
+++ ent/Player.hpp
@@ -6,19 +6,25 @@
 #include <string>
 
 namespace entity {
-namespace mob { 
-	
+namespace mob {
+
 	enum Class { RAIDER, MARINER, SORCERER, TECH_MASTER, BIO_PRO };
 	std::string className(Class cn);
-	
-	class Player : public Mob { 
-		
+
+	class Player : public Mob {
+
 	public:
 		Player(Class c, std::string username);
 		void input();
 		void render() override;
+
+	private:
+		std::string p_username;
+		Class p_class;
+		int p_x, p_y;
+		int id;
 	};
-}	
+}
 }
 
 #endif
