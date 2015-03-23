a7dbf51de53b65d38762d7a34b44ffb279b20ec0
Microcoding <cjaf.microcode@outlook.com>
2015-03-22 22:27:47 -0400

    add functions

--- ent/Player.cpp
+++ ent/Player.cpp
@@ -1,3 +1,35 @@
 #include "Player.hpp"
 #include "Entity.hpp"
 #include "Mob.hpp"
+#include <string>
+
+namespace entity {
+namespace mob {
+
+    std::string className(Class cn) {
+        switch (cn) {
+            case RAIDER:
+                return "RAIDER";
+                break;
+            case MARINER:
+                return "MARINER";
+                break;
+            case SORCERER:
+                return "SORCERER";
+                break;
+            case TECH_MASTER:
+                return "TECH MASTER";
+                break;
+            case BIO_PRO:
+                return "BIO PRO";
+                break;
+        }
+        return null;
+    }
+
+    Player::Player(Class c, std::string username) : Mob(Player::id, Player::p_speed, Player::p_health) { 
+        Player::p_class = c;
+        Player::p_username = username;
+    }
+}
+}
