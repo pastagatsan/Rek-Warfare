4fc3a93613349ec1b571d67d6700a4caedea9098
Rodrun <juanmendg@outlook.com>
2015-06-08 19:14:10 -0400

    Remove redundant "Entity::" I forget the name of it, is it prefix? Anyways, also replaced spaces with tabs.

--- ent/Entity.cpp
+++ ent/Entity.cpp
@@ -1,28 +1,12 @@
 #include "Entity.hpp"
 #include "SDL2/SDL.h"
-#include "SDL2/SDL_image.h"
-#include "graphics/Drawer.hpp"
 
 namespace entity {
 
-Entity::Entity(SDL_Rect* box) : e_box(box) { }
+Entity::Entity(SDL_Rect box) : m_box(box) { }
+
+Entity::~Entity() { }
 
 void Entity::render() { }
-
-void Entity::setX(int x) {
-	Entity::e_box->x = x;
-}
-
-void Entity::setY(int y) {
-	Entity::e_box->y = y;
-}
-
-int Entity::getX() {
-	return Entity::e_box->x;
-}
-
-int Entity::getY() {
-	return Entity::e_box->y;
-}
 
 }
