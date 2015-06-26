d76e3c2f467121e78c1655e69041ededd45dadd0
Juan <juan@localhost.localdomain>
2015-06-25 20:50:59 -0400

    No longer require SDL_Rect to be provided It's a lot easier to just give simple values instead of creating SDL_Rect in places you don't want to. Also: Replace spaces with indents.

--- graphics/Drawer.cpp
+++ graphics/Drawer.cpp
@@ -9,32 +9,35 @@
 namespace drawer {
 
 SDL_Texture* loadTexture(SDL_Renderer* rend, std::string path) {
-    SDL_Texture* tex = nullptr;
-    SDL_Surface* load = IMG_Load(path.c_str());
+	SDL_Texture* tex = nullptr;
+	SDL_Surface* load = IMG_Load(path.c_str());
 
-    if (load == nullptr) {
-        std::cerr << "Could not load " << path << "!Reason: " << SDL_GetError() << std::endl;
-    } else {
-        tex = SDL_CreateTextureFromSurface(rend, load);
-        if (tex == nullptr) {
-            std::cerr << "Couldn't create texture from surface " << path << "!Reason: "
+	if (load == nullptr) {
+		std::cerr << "Could not load " << path << "!Reason: " << SDL_GetError() << std::endl;
+	} else {
+		tex = SDL_CreateTextureFromSurface(rend, load);
+		if (tex == nullptr) {
+			std::cerr << "Couldn't create texture from surface " << path << "!Reason: "
 				<< SDL_GetError() << std::endl;
-        }
-        SDL_FreeSurface(load);
-    }
-    return tex;
+		}
+		SDL_FreeSurface(load);
+	}
+	return tex;
 }
 
-void drawTexture(SDL_Renderer* rend, SDL_Texture* texture, SDL_Rect* fraction, SDL_Rect* size) {
-    SDL_RenderCopy(rend, texture, fraction, size);
+void drawTexture(SDL_Renderer* rend, SDL_Texture* texture, int x, int y int w, int h,
+	int xoff, int yoff, int tileW, int tileH) {
+    SDL_Rect fraction = { xoff, yoff, tileW, tileH };
+    SDL_Rect size = { x, y, w, h };
+	SDL_RenderCopy(rend, texture, &fraction, &size);
 }
 
 void setColor(SDL_Texture* texture, Uint8 r, Uint8 g, Uint8 b) {
-    SDL_SetTextureColorMod(texture, r, g, b);
+	SDL_SetTextureColorMod(texture, r, g, b);
 }
 
 void setAlpha(SDL_Texture* texture, Uint8 alpha) {
-    SDL_SetTextureAlphaMod(texture, alpha);
+	SDL_SetTextureAlphaMod(texture, alpha);
 }
 
 }
--- graphics/Drawer.hpp
+++ graphics/Drawer.hpp
@@ -7,16 +7,8 @@
 namespace drawer {
 
 	SDL_Texture* loadTexture(SDL_Renderer* rend, std::string path);
-    /**
-     * Renders a texture.
-     *
-     * rend - SDL_Renderer
-     * texture - the texture
-     * fraction - part of the texture to be rendered
-     * size - coordinates, width, and height
-     */
-	void drawTexture(SDL_Renderer* rend, SDL_Texture* texture, SDL_Rect* fraction=nullptr,
-		SDL_Rect* size=nullptr);
+	void drawTexture(SDL_Renderer* rend, SDL_Texture* texture, int x, int y
+		int w, int h, int xoff = -1, int yoff = -1, int tileW = -1, int tileH = -1);
 	void setColor(SDL_Texture* texture, Uint8 r=0, Uint8 g=0, Uint8 b=0);
 	void setAlpha(SDL_Texture* texture, Uint8 alpha=1);
 }
