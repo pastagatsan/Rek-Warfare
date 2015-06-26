ebb113d19eab1118d2508ed29b1e7ae6a95bff40
Juan <juan@localhost.localdomain>
2015-06-25 20:54:29 -0400

    Add missing comma

--- graphics/Drawer.hpp
+++ graphics/Drawer.hpp
@@ -7,7 +7,7 @@
 namespace drawer {
 
 	SDL_Texture* loadTexture(SDL_Renderer* rend, std::string path);
-	void drawTexture(SDL_Renderer* rend, SDL_Texture* texture, int x, int y
+	void drawTexture(SDL_Renderer* rend, SDL_Texture* texture, int x, int y,
 		int w, int h, int xoff = -1, int yoff = -1, int tileW = -1, int tileH = -1);
 	void setColor(SDL_Texture* texture, Uint8 r=0, Uint8 g=0, Uint8 b=0);
 	void setAlpha(SDL_Texture* texture, Uint8 alpha=1);
