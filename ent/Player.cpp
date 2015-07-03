5fb8ba0d905659d2ba7a64134ca889dd4d6864af
rodrun <juanmendg@outlook.com>
2015-07-02 23:04:34 -0400

    Implement key input handling

--- ent/Player.cpp
+++ ent/Player.cpp
@@ -1,13 +1,14 @@
 #include "Player.hpp"
 #include "Entity.hpp"
 #include "Mob.hpp"
+#include "SDL2/SDL.h"
 
 #include <string>
 
 namespace entity {
 namespace mob {
 
-	std::string className(Class cn) {
+	std::string classNameToStr(Class cn) {
 		switch (cn) {
 			case RAIDER:
 				return "RAIDER";
@@ -31,7 +32,21 @@ namespace mob {
 	Player::Player(Class c, std::string username)
 		: Mob(m_id, m_box, m_speed, m_health), m_class(c), m_username(username) { }
 
-	void Player::input() { }
-	void Player::render() { }
+	void Player::update() { }
+
+	void Player::input() {
+		const Uint8* key = SDL_GetKeyboardState(nullptr);
+		if (key[SDL_SCANCODE_W]) {
+			// TODO: Decide weather SPACE or W wi		return ll be the default jump key
+		} else if (key[SDL_SCANCODE_S]) {
+
+		}
+		if (key[SDL_SCANCODE_A]) {
+
+		} else if (key[SDL_SCANCODE_D]) {
+			// Shouldn't be messed with until ladders come about
+		}
+	}
+	void Player::render(SDL_Renderer* rend) const { }
 }
 }
