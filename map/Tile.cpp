b9f458bdf2fd4cd148e50226848f33cf9a597d0b
rodrun <juanmendg@outlook.com>
2015-07-03 10:40:57 -0400

    Add derived functions

--- map/Tile.cpp
+++ map/Tile.cpp
@@ -4,7 +4,10 @@
 namespace entity {
 namespace map {
 
-	Tile::Tile(ID id) : m_id(id) { }
-	Tile::Tile(ID id, Mask mask) : m_id(id), m_mask(mask) { }
+	Tile::Tile(ID id) : Entity(m_box), m_id(id) { }
+	Tile::Tile(ID id, Mask mask) : Entity(m_box), m_id(id), m_mask(mask) { }
+
+	void Tile::update() { }
+	void Tile::render(SDL_Renderer* rend) const { }
 }
 }
