7e1a17ffaa4e9b695396cb55109f7c8a37a51fd0
rodrun <juanmendg@outlook.com>
2015-07-03 11:38:40 -0400

    Initial push of in-game GUI

---
+++ gui/HUD.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "SDL2/SDL.h"
+
+#include <string>
+
+namespace gui {
+
+	class HUD {
+
+	public:
+		HUD();
+	};
+}
---
+++ gui/Item.hpp
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "../ent/Entity.hpp"
+#include "SDL2/SDL.h"
+
+namespace gui {
+
+	class Item : public entity::Entity {
+
+	public:
+		Item();
+	};
+}
