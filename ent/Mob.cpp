7d48ff013914eddeee185cd5b5770f4e21be81f9
rodrun <juanmendg@outlook.com>
2015-07-02 23:03:51 -0400

    Handle move

--- ent/Mob.cpp
+++ ent/Mob.cpp
@@ -4,14 +4,32 @@
 namespace entity {
 namespace mob {
 
-Mob::Mob(int id, SDL_Rect box, int speed, int health): Entity(box) {
+Mob::Mob(int id, SDL_Rect box, int speed, int health): Entity(box, id) {
 	Mob::m_id = id;
 	Mob::m_speed = speed;
-	Mob::m_hp = health;
+	Mob::m_health = health;
 }
 
-Mob::~Mob() { }
-void Mob::render() { }
+void Mob::move(Direction dir, int speed) {
+	switch (dir) {
+		case UP:
+			m_box.y -= speed;
+			break;
+		case DOWN:
+			m_box.y += speed;
+			break;
+		case LEFT:
+			m_box.x -= speed;
+			break;
+		case RIGHT:
+			m_box.y += speed;
+			break;
+	}
+}
+
+void Mob::move(Direction dir) {
+	move(dir, m_speed);
+}
 
 }
 }
--- ent/Mob.hpp
+++ ent/Mob.hpp
@@ -6,18 +6,20 @@
 namespace entity {
 namespace mob {
 
+	enum Direction { UP, DOWN, LEFT, RIGHT };
+
 	class Mob : public Entity {
 
 	public:
 		Mob(int id, SDL_Rect box, int speed, int health);
-		virtual ~Mob();
-		virtual void render() override;
+		void move(Direction dir, int speed);
+		// Move with m_speed
+		void move(Direction dir);
 		int getSpeed() { return m_speed; }
-		double getHP() { return m_hp; }
+		double getHealth() { return m_health; }
 	protected:
-		int m_speed;
-		double m_hp;
-		SDL_Rect m_box;
+		int m_speed = 0;
+		double m_health = 0;
 	};
 }
 }
