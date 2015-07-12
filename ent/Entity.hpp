744ddfee8c429ca4ae884f3ab64f1c1b053286f6
rodrun <juanmendg@outlook.com>
2015-07-11 23:15:37 -0400

    Never forget width and height

--- ent/Entity.hpp
+++ ent/Entity.hpp
@@ -13,9 +13,13 @@ namespace entity {
 		virtual void render() const { }
 		void setX(int x) { m_box.x = x; }
 		void setY(int y) { m_box.y = y; }
+		void setW(int w) { m_box.w = w; }
+		void setH(int h) { m_box.h = h; }
 		void setRenderer(SDL_Renderer*& rend) { m_rend = rend; }
-		int getX() { return m_box.x; }
-		int getY() { return m_box.y; }
+		int getX() const { return m_box.x; }
+		int getY() const { return m_box.y; }
+		int getW() const { return m_box.w; }
+		int getH() const { return m_box.h; }
 		SDL_Renderer* getRenderer() { return m_rend; }
 
 	protected:
