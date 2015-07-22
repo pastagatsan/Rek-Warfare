abda3f6cd14898564426ebec2cf03debb9426427
rodrun <juanmendg@outlook.com>
2015-07-22 19:51:51 -0400

    Use entity namespace and add functions Functions for coordinates in relation to it's parent container

--- gui/Item.cpp
+++ gui/Item.cpp
@@ -2,6 +2,10 @@
 #include "../ent/Entity.hpp"
 #include "SDL2/SDL.h"
 
+namespace entity {
 namespace gui {
-	Item::Item(SDL_Renderer* rend) : entity::Entity(rend, m_box) { }
+	Item::Item(SDL_Renderer* rend) : Entity(rend, m_box) { }
+
+	Item::~Item() { }
+}
 }
--- gui/Item.hpp
+++ gui/Item.hpp
@@ -3,14 +3,20 @@
 #include "../ent/Entity.hpp"
 #include "SDL2/SDL.h"
 
+namespace entity {
 namespace gui {
 
-	class Item : public entity::Entity {
+	class Item : public Entity {
 
 	public:
 		Item(SDL_Renderer* rend);
-		void update() override;
-		void render() const override;
-	private:
+		~Item();
+		void setRX(int rx) { m_rbox.x = rx; }
+		void setRY(int ry) { m_rbox.y = ry; }
+		int getRX() const { return m_rbox.x; }
+		int getRY() const { return m_rbox.y; }
+	protected:
+		SDL_Rect m_rbox;
 	};
+}
 }
