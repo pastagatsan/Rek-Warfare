67a1a1ab4321cccc667ad23799c2e6f027d5d5fe
rodrun <juanmendg@outlook.com>
2015-07-22 19:55:03 -0400

    Handle item adding and background handling No rendering just yet, need to have items that are functional.

--- gui/Panel.hpp
+++ gui/Panel.hpp
@@ -2,24 +2,28 @@
 #pragma once
 
 #include "../ent/Entity.hpp"
+#include "Item.hpp"
+#include "SDL2/SDL.h"
 
-#include <vector>
-
+namespace entity {
 namespace gui {
 
-	class Panel : public entity::Entity {
+	class Panel : public Entity {
 
 	public:
 		Panel(SDL_Renderer* rend, int x, int y, int w, int h);
-		~Panel() override;
 		void update() override;
 		void render() const override;
+		void setBackground(Uint8 c[3]);
+		// Coordinates should be in relation with the panel dimensions
+		// rx <= panel.width && rx + item.width <= panel.width
+		void add(Item*& item, int rx, int ry, int rw, int rh);
 	private:
-		std::vector<Item> m_items;
-	public:
-		// Protable?
-		size_t itemCount() const { return m_items.size(); }
+		Item* m_items[256];
+		int i_itemCount = 0;
+		Uint8 m_bg[3] = { 0, 0, 0 };
 
 	};
 
+}
 }
