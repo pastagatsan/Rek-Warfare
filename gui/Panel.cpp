6b6b168f31d1c31095043b82d5ca2bcd57ed57e9
rodrun <juanmendg@outlook.com>
2015-07-24 22:31:18 -0400

    Panel enhancement Check if item list is full and if item added is bigger or outside of the parent Panel

--- gui/Panel.cpp
+++ gui/Panel.cpp
@@ -2,8 +2,9 @@
 #include "../ent/Entity.hpp"
 #include "Item.hpp"
 #include "SDL2/SDL.h"
+#include <string>
+#include "../Log.hpp"
 
-			#include <iostream>
 namespace entity {
 namespace gui {
 	Panel::Panel(SDL_Renderer* rend, int x, int y, int w, int h)
@@ -36,12 +37,25 @@ namespace gui {
 	}
 
 	void Panel::add(Item* item, int rx, int ry, int w, int h) {
-		item->setRX(rx);
-		item->setRY(ry);
-		item->setW(w);
-		item->setH(h);
-		m_items[i_itemCount] = item;
-		i_itemCount++;
+		if (!(i_itemCount >= 256)){
+			if ((rx + w > m_box.w) || (ry + h > m_box.h) ||
+				(rx < m_box.x) || (ry < m_box.y)) {
+				logger::log(logger::WARNING, "Item #" + std::to_string(i_itemCount)
+					+ " is out of bounds! (Panel " + m_name + ")");
+			} else {
+				item->setRX(rx);
+				item->setRY(ry);
+				item->setCoordinates(m_box.x + rx, m_box.y + ry);
+				item->setSize(w, h);
+			}
+
+			m_items[i_itemCount] = item;
+			i_itemCount++;
+		} else {
+			logger::log(logger::ERROR, "Panel" + m_name " full! (limit = "
+				+ std::to_string(MAX_ITEMS) + ")");
+			return;
+		}
 	}
 }
 }
--- gui/Panel.hpp
+++ gui/Panel.hpp
@@ -8,6 +8,8 @@
 namespace entity {
 namespace gui {
 
+	const auto MAX_ITEMS = 256;
+
 	class Panel : public Entity {
 
 	public:
@@ -15,16 +17,21 @@ namespace gui {
 		void update() override;
 		void render() const override;
 		void setBackgroundEnabled(bool flag) { m_backgroundEnabled = flag; }
-		void setBackground(Uint8 c[3]);
+		void setBackgroundColor(Uint8 c[3]);
+		void setBackgroundImage(SDL_Texture* bg);
 		// Coordinates should be in relation with the panel dimensions
 		// rx <= panel.width && rx + item.width <= panel.width
 		void add(Item* item, int rx, int ry, int w, int h);
 		bool backgroundEnabled() const { return m_backgroundEnabled; }
+		// Set "panel name" for logging purposes. Not required, though
+		void setName(std::string name) { }
 	private:
-		Item* m_items[256];
+		Item* m_items[MAX_ITEMS];
 		int i_itemCount = 0;
 		Uint8 m_bg[3] = { 0, 0, 0 };
 		bool m_backgroundEnabled = true;
+		SDL_Texture* m_backgroundImage = nullptr;
+		std::string m_name = "Unnamed";
 
 	};
 
