4068f7d559da61bb1b49e5e44c6f25b380fc3853
rodrun <juanmendg@outlook.com>
2015-07-24 23:19:43 -0400

    Actually implement name

--- gui/Panel.hpp
+++ gui/Panel.hpp
@@ -4,6 +4,7 @@
 #include "../ent/Entity.hpp"
 #include "Item.hpp"
 #include "SDL2/SDL.h"
+#include <string>
 
 namespace entity {
 namespace gui {
@@ -24,11 +25,12 @@ namespace gui {
 		void add(Item* item, int rx, int ry, int w, int h);
 		bool backgroundEnabled() const { return m_backgroundEnabled; }
 		// Set "panel name" for logging purposes. Not required, though
-		void setName(std::string name) { }
+		void setName(std::string name) { m_name = name; }
 	private:
 		Item* m_items[MAX_ITEMS];
 		int i_itemCount = 0;
 		Uint8 m_bg[3] = { 0, 0, 0 };
+		bool m_backgroundImageEnabled = false;
 		bool m_backgroundEnabled = true;
 		SDL_Texture* m_backgroundImage = nullptr;
 		std::string m_name = "Unnamed";
